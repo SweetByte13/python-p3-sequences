0c512088184d86c30261be170724f2978cd2986b
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-05-14 09:31:26 -0400

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci=[0,1,1,2,3,5,8,13,21,34]
+    if length==1:
+        print([fibonacci[0]])
+    elif length==2:
+        print(fibonacci[0:2])
+    elif length == 10:
+        print(fibonacci[0:10])
+    else:
+        print([])
